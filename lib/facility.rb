27484d2041708b12fc362b07727e30f1e1bb9278
William Ross <bill_87_2@yahoo.com>
2025-04-02 14:46:23 -0500

    Commit facility passes tests

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,17 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize((name, address, phone))
-    @name = name
-    @address = address
-    @phone = phone
-    @services = []
+  def initialize(attributes)
+    @name = attributes[:name]
+    @address = attributes[:address]
+    @phone = attributes[:phone]
+    @services = attributes[:services] || []
+  
   end
 
   def add_service(service)
-    @services << service
+    @services.append(service)
   end
+
+  
 end
